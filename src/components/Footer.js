5af2682e29a84cd0d9fa1457691aa7c405924c6e
Rajeshdreams333 <rajeshdreams333@gmail.com>
2023-12-16 00:48:12 +0530

    component file added

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-200 p-4 text-center'>
+      <div>
+        <h1 className='font-bold'>Follow Us</h1>
+        <div className='flex justify-center mt-4'>
+          <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer' className='mx-2'>
+            <FontAwesomeIcon icon={faFacebook} size='2x' />
+          </a>
+          <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer' className='mx-2'>
+            <FontAwesomeIcon icon={faTwitter} size='2x'/>
+          </a>
+          <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer' className='mx-2'>
+            <FontAwesomeIcon icon={faInstagram} size='2x' />
+          </a>
+        </div>
+      </div>
+      <p className='mt-4'>&copy; 2023 BeeData</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='p-2 flex  items-center bg-lime-200 shadow-md'>
+      <div className="flex-none">
+      <img src="./assets/tasty-log.png" alt="logo" className='h-16'/>
+      </div>
+      <div className='flex-grow text-center'>
+        <div>
+      <h1 className='font-bold text-4xl'>Tassty-Feast😋</h1>
+      </div>
+      </div>
+      <div className='p-2'>
+        <ul className='flex space-x-3 font-bold text-base'>
+          <li>Home</li>
+          <li>Ingredients</li>
+          <li>Nutrition</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
